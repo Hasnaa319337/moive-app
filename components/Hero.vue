efb01db71286c87919617a6684bfe69d884e1afb
Hasnaa Adel <hasnaa3193378@gmail.com>
2022-11-15 11:51:32 +0000

    m

---
+++ components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+    <div div class="hero">
+       
+        <div class="text-container" >
+            <div class="text">
+                <span class="mini-heading">NOW STREAMING</span>
+                <h1><span>now</span>STREAMING</h1>
+                <a href="#moiveGrid">View Moives</a>
+            </div>
+        </div>
+
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,22 +1,17 @@
 <template>
   <div>
     <div class="container">
-      <h2>products</h2>
-      <div class="row">
-        <div class="col-sm-6 col-md-4 col-lg-3" v-for="p in products" :key="p.id">
-            <NuxtLink :to="`/products/${p.id}`">{{p.title}}</NuxtLink>
-        </div>
-      </div>
-    </div>
+      <Hero/>
+  </div>
   </div>
 </template>
 
 <script setup>
 definePageMeta({
-  layout: "products",
+
 });
 //fetch data
-const { data: products } = await useFetch("https://fakestoreapi.com/products");
+
 </script>
 
 <style></style>
