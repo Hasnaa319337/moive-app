163c0977cda807dbeb6f1ef6096ac57931ab3354
hasnaa-ux <58074521+hasnaa-ux@users.noreply.github.com>
2022-11-15 11:34:24 +0200

    m

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,4 +37,14 @@ li {
 a {
   text-decoration: none;
 }
+
+.layout-enter-active,
+.layout-leave-active {
+  transition: all 0.4s;
+}
+.layout-enter-from,
+.layout-leave-to {
+  filter: grayscale(1);
+}
+
 </style>
