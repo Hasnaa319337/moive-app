60b17ebfa933d6762788f9c64b8763fac7ab7f36
hasnaa-ux <58074521+hasnaa-ux@users.noreply.github.com>
2022-11-15 11:32:59 +0200

    c1

--- pages/about.vue
+++ pages/about.vue
@@ -14,4 +14,14 @@
 
 <script setup></script>
 
-<style></style>
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
